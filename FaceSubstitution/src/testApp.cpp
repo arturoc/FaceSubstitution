416be4056e510034fc721df880cc151ffce79242
arturo <arturo@openframeworks.cc>
2013-09-05 14:50:56 +0200

    change camera resolution

--- FaceSubstitution/src/testApp.cpp
+++ FaceSubstitution/src/testApp.cpp
@@ -8,7 +8,7 @@ void testApp::setup() {
 #endif
 	ofSetVerticalSync(true);
 	cloneReady = false;
-	cam.initGrabber(1280, 720);
+	cam.initGrabber(960,544);
 	clone.setup(cam.getWidth(), cam.getHeight());
 	ofFbo::Settings settings;
 	settings.width = cam.getWidth();
