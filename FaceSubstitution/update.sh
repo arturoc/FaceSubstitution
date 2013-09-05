c2959951fcf85de03daf3d673de3c2499f8a5ee7
arturo <arturo@openframeworks.cc>
2013-09-05 12:21:51 +0200

    update.sh fixes

--- FaceSubstitution/update.sh
+++ FaceSubstitution/update.sh
@@ -1,6 +1,7 @@
-git pull origin installation_simple --dry-run | grep -q -v 'Already up-to-date.' && changed=1 
-if [ changed -eq 1 ]; then
-    make
+changed=0
+git pull origin installation_simple | grep -q -v 'Already up-to-date.' && changed=1 
+if [ $changed -eq 1 ]; then
+    make > make.log
     ret=$?
     if [ $ret -eq 0 ]; then
         killall FaceSubstitution
